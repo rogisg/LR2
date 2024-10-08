751f307e2ef7431dbd4eebfed8b48a386af4d00e
rogisg <gogalebedev91@gmail.com>
2024-10-08 13:02:04 +0300

    task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,29 @@
+//задача 3
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"1 x 7 = 7"<<endl;
+    cout<<"2 x 7 = 14"<<endl;
+    cout<<"3 x 7 = 21"<<endl;
+    cout<<"4 x 7 = 28"<<endl;
+    cout<<"5 x 7 = 35"<<endl;
+    cout<<"6 x 7 = 42"<<endl;
+    cout<<"7 x 7 = 49"<<endl;
+    cout<<"8 x 7 = 56"<<endl;
+    cout<<"9 x 7 = 63"<<endl;
+    int n;
+    cin >> n;
+    cout << "1 x " << n << " = " << 1 * n << endl;
+    cout << "2 x " << n << " = " << 2 * n << endl;
+    cout << "3 x " << n << " = " << 3 * n << endl;
+    cout << "4 x " << n << " = " << 4 * n << endl;
+    cout << "5 x " << n << " = " << 5 * n << endl;
+    cout << "6 x " << n << " = " << 6 * n << endl;
+    cout << "7 x " << n << " = " << 7 * n << endl;
+    cout << "8 x " << n << " = " << 8 * n << endl;
+    cout << "9 x " << n << " = " << 9 * n << endl;
+    
+    
+return 0;
+}
